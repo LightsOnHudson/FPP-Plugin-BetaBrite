3ef588556bdd9e8f77e638edcffc9de154c1bf2f
LightsOnHudson <bshaver@MBPRO-TDL.home.lan>
2014-12-10 08:31:20 -0700

    remote fpp_install

--- fpp_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /tmp
-/bin/mkdir /opt/fpp/plugins/BetaBrite
-/usr/bin/wget http://50.194.140.209/fpp/plugins/BetaBrite/BetaBrite.zip
-/usr/bin/unzip BetaBrite.zip -d /opt/fpp/plugins/BetaBrite
-/bin/rm /tmp/BetaBrite.zip
-/bin/chmod +x /opt/fpp/plugins/BetaBrite/callbacks.php
